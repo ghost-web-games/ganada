0f57d2b842105480125c31d18ccbfc67a75cf855
zcbm4321 <zcbm4321@gmail.com>
2023-12-22 10:52:36 +0900

    add powered by

--- src/scene/startscene.ts
+++ src/scene/startscene.ts
@@ -8,12 +8,14 @@ export default class StartScene implements IScene {
     readyScreen: HTMLElement
     playBtn: HTMLElement
     title: HTMLElement
+    help: HTMLElement
     centerPos: Vector
     drawObject: Array<IDraw>
 
     constructor(private factory: AppFactory, private scene: IScene) {
         this.readyScreen = document.querySelector('.ready-screen') as HTMLElement
         this.title = this.readyScreen.querySelector('.title-text') as HTMLElement
+        this.help = this.readyScreen.querySelector('.help-text') as HTMLElement
         this.playBtn = this.readyScreen.querySelector('.play-img') as HTMLElement
         this.playBtn.addEventListener('click', this.nextScene.bind(this))
         this.centerPos = new Vector(0, 0)
@@ -31,6 +33,9 @@ export default class StartScene implements IScene {
         gsap.to(this.title, {
             scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5
         })
+        gsap.to(this.help, {
+            scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5
+        })
     }
     gameRelease() {
         this.drawObject.length = 0
@@ -43,6 +48,9 @@ export default class StartScene implements IScene {
             }
         })
         gsap.to(this.title, {
+            opacity: 0, pointerEvents: 'none', duration: 0.3, onComplete: () => {}
+        })
+        gsap.to(this.help, {
             opacity: 0, pointerEvents: 'none', duration: 0.3, onComplete: () => {}
         })
     }
